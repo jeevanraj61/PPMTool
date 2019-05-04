2a0763da7ae0f7197773fcef3c9ed17f8ccf6cf9
jeevanraj61 <jeevanraj61@gmail.com>
2019-05-04 15:40:22 +0530

    react

---
+++ src/main/resources/static/precache-manifest.2a9528d4aad830dd83f90d9722904b03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ab16aafc363c1cfa9bd9",
+    "url": "/static/js/main.34313697.chunk.js"
+  },
+  {
+    "revision": "67434a94b3cdfeba1a58",
+    "url": "/static/js/2.503f48b6.chunk.js"
+  },
+  {
+    "revision": "ab16aafc363c1cfa9bd9",
+    "url": "/static/css/main.1cd40a5a.chunk.css"
+  },
+  {
+    "revision": "67434a94b3cdfeba1a58",
+    "url": "/static/css/2.aa3ba310.chunk.css"
+  },
+  {
+    "revision": "c10b0bfb27cb5d601df32d7ea7edd8a9",
+    "url": "/index.html"
+  }
+];
